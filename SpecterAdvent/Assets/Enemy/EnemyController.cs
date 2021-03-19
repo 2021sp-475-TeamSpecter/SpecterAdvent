1328d95d66fb3b02e913ec1baf63b8640772c322
Amy Burnett <16aburnett@gmail.com>
2021-03-18 23:02:28 -0400

    Player and Enemy movement

---
+++ SpecterAdvent/Assets/Enemy/EnemyController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    // Health 
+    [Header( "Health" )]
+    public float maxHealth = 100;
+    public float currentHealth = 100;
+    public RectTransform healthBar; 
+    // Health decrease over time
+    public float decreaseTime = 1;
+    public float prevTime; 
+
+    [Header( "Movement" )]
+    public float movementSpeed = 1;
+    public Rigidbody2D rigidbody;
+    public Transform spriteTransform; 
+    public GameObject player;
+
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        rigidbody = GetComponent<Rigidbody2D>();
+        // Keep track of player
+        player = GameObject.Find("Player"); 
+        
+    }
+
+    void Update()
+    {
+        // adjust healthbar 
+        // healthbar is 200 wide 
+        // convert health level to bar width 
+        float healthPercent = currentHealth / maxHealth; 
+        float barLevel = healthPercent * 200;
+        healthBar.sizeDelta = new Vector2(barLevel, healthBar.sizeDelta.y);
+        
+        // gradually decrease health 
+        if (Time.time > prevTime + decreaseTime)
+        {
+            prevTime = Time.time;
+            currentHealth -= 5;
+        }
+
+        // move towards player
+        Vector3 directionToPlayer = player.transform.position - transform.position;
+        rigidbody.velocity = directionToPlayer.normalized * movementSpeed;
+
+    }
+}
---
+++ SpecterAdvent/Assets/Scripts/PlayerController2D.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2D : MonoBehaviour
+{
+	Animator animator;
+	Rigidbody2D rb2d;
+	SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	animator = GetComponent<Animator>();
+    	rb2d = GetComponent<Rigidbody2D>();
+    	spriteRenderer = GetComponent<SpriteRenderer>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	if(Input.GetKey("d") || Input.GetKey("right"))
+    	{
+    		rb2d.velocity = new Vector2(2, rb2d.velocity.y);
+    		animator.Play("Walking");
+    		spriteRenderer.flipX = false;
+    	}
+        else if(Input.GetKey("a") || Input.GetKey("left"))
+    	{
+    		rb2d.velocity = new Vector2(-2, rb2d.velocity.y);
+    		animator.Play("Walking");
+    		spriteRenderer.flipX = true;
+    	}
+    	else
+    	{
+    		animator.Play("Idle");
+    	}
+
+    	if(Input.GetKey("space"))
+    	{
+    		rb2d.velocity = new Vector2(rb2d.velocity.x, 3);
+    		animator.Play("Jumping");
+    	}
+    }
+}
